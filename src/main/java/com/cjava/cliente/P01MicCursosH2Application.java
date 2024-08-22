c4673d68f608093d89d85b586a9996cdca437bf9
Geraldine López <geraldinelopez@MacBook-Pro-de-Geraldine-2.local>
2024-08-22 04:25:39 +0200

    Cursos con H2

--- src/main/java/com/cjava/cliente/P01MicCursosH2Application.java
+++ src/main/java/com/cjava/cliente/P01MicCursosH2Application.java
@@ -2,7 +2,9 @@ package com.cjava.cliente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient // Para que se registre en el servidor Eureka
 @SpringBootApplication
 public class P01MicCursosH2Application {
 
